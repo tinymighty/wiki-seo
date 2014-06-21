d3254cf2c3100448575430d56825f20c6e489ef8
Andru Vallance <andru@tinymighty.com>
2014-06-21 08:46:04 +0200

    Fixed title_mode parsing and added new property title_separator to define a separator when using append or prepend modes.

--- WikiSEO.body.php
+++ WikiSEO.body.php
@@ -12,6 +12,7 @@ class WikiSEO{
 	protected static 	$valid_params = array(
 		'title',
 		'title_mode', //append, prepend, replace
+		'title_separator',
 		'keywords',
 		'description',
 		'google-site-verification',
@@ -69,9 +70,16 @@ class WikiSEO{
 	//parameters which should be parsed if possible to allow for the expansion of templates
 	protected static  $parse_params = array('title', 'description', 'keywords');
 
+	//the value for the html title tag
 	protected static 	$title = '';
+	//prepend, append or replace the new title to the existing title
 	protected static 	$title_mode = 'replace';
+	//the separator to use when using append or prepend modes
+	protected static  $title_separator = ' - ';
+
+	//array of meta name values
 	protected static 	$meta = array();
+	//array of meta property values
 	protected static 	$property = array();
 
 	//do not allow this class to be instantiated, it is static
@@ -157,7 +165,7 @@ class WikiSEO{
 		//ensure only valid parameter names are processed
 		foreach(self::$valid_params as $p){
 			if( isset($params[$p]) ){
-				//if the parser has been passed and the param is parsable parse it, else simply assign in
+				//if the parser has been passed and the param is parsable parse it, else simply assign it
 				$processed[$p] = ($parser && in_array($p, self::$parse_params)) ? $parser->recursiveTagParse($params[$p]) : $params[$p];
 			}
 		}
@@ -167,8 +175,12 @@ class WikiSEO{
 				self::$title = $v;
 			}
 			else
-			if( $k==='title_mode' && in_array($k, self::$valid_title_modes) ){
+			if( $k==='title_mode' && in_array($v, self::$valid_title_modes) ){
 				self::$title_mode = $v;
+			}
+			else
+			if( $k === 'title_separator'){
+				self::$title_separator = ' '.$v.' ';
 			}
 			else
 			if( isset(self::$tag_types[$k]) && self::$tag_types[$k]==='meta' ){
@@ -241,10 +253,10 @@ class WikiSEO{
 		if(!empty(self::$title)){
 			switch(self::$title_mode){
 				case 'append':
-					$title = $out->getPageTitle() . ' - ' . self::$title;
+					$title = $out->getPageTitle() . self::$title_separator . self::$title;
 					break;
 				case 'prepend':
-					$title = self::$title . ' - ' . $out->getPageTitle();
+					$title = self::$title . self::$title_separator . $out->getPageTitle();
 					break;
 				case 'replace':
 				default:
