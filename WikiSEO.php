54be2c07d57618b0be2d9eba0ddfb886e944501a
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-12-11 20:10:46 +0100

    Update WikiSEO.php
    * Provide license information
    * Tweaks to file documentation

--- WikiSEO.php
+++ WikiSEO.php
@@ -1,10 +1,11 @@
 <?php
 /**
  * WikiSEO extension - Allows per page meta settings (keywords, description) and to change the title.
- * @version 1.2.0 - 2014/11/10 (based on the work of Vladimir Radulovski and Jim Wilson)
+ * @version 1.2.1 - 2014/12/11 (based on the work of Vladimir Radulovski and Jim Wilson)
  *
  * @link https://www.mediawiki.org/wiki/Extension:WikiSEO Documentation
  * @link https://www.mediawiki.org/wiki/Extension_talk:WikiSEO Support
+ * @link https://github.com/tinymighty/wiki-seo/issues Bug tracker
  * @link https://github.com/andru/wiki-seo Source Code
  *
  * @file
@@ -15,13 +16,14 @@
  * @license https://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
-// Confirm MW environment
+// Confirm MediaWiki environment.
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'This file is a MediaWiki extension and thus not a valid entry point.' );
 }
 
 global $wgExtensionCredits, $wgAutoloadClasses, $wgExtensionMessagesFiles, $wgHooks;
-// Credits
+
+// Display extension properties on MediaWiki.
 $wgExtensionCredits['parserhook'][] = array(
 	'path' => __FILE__,
 	'name' => 'WikiSEO',
@@ -30,10 +32,14 @@ $wgExtensionCredits['parserhook'][] = array(
 		),
 	'url' =>'https://www.mediawiki.org/wiki/Extension:WikiSEO',
 	'descriptionmsg' => 'seo-desc',
-	'version'=>'1.2.0'
+	'license-name' => 'GPL-2.0+',
+	'version'=>'1.2.1'
 );
 
+// Register extension class.
 $wgAutoloadClasses['WikiSEO'] = dirname(__FILE__) . '/WikiSEO.body.php';
+
+// Register extension messages and other localisation.
 $wgExtensionMessagesFiles['WikiSEOMagic'] = dirname( __FILE__ ) . '/WikiSEO.i18n.magic.php';
 $wgExtensionMessagesFiles['WikiSEO'] = dirname( __FILE__ ) . '/WikiSEO.i18n.php';
 
