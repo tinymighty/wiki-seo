3747b7184e7698d8a71e0cfb5ec491d30af44c62
Ike Hecht <tosfos@yahoo.com>
2015-12-18 11:15:35 -0800

    Add extension.json

--- WikiSEO.php
+++ WikiSEO.php
@@ -15,39 +15,52 @@
  * @copyright (C) 2013 Andru Vallance
  * @license https://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
-
 // Confirm MediaWiki environment.
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'This file is a MediaWiki extension and thus not a valid entry point.' );
 }
 
-global $wgExtensionCredits, $wgAutoloadClasses, $wgExtensionMessagesFiles, $wgHooks;
+global $wgExtensionMessagesFiles;
 
-// Display extension properties on MediaWiki.
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'WikiSEO',
-	'author' => array(
-		'Andru Vallance', '...'
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'WikiSEO' );
+
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgExtensionMessagesFiles['WikiSEOMagic'] = __DIR__ . '/WikiSEO.i18n.magic.php';
+	$wgExtensionMessagesFiles['WikiSEO'] = __DIR__ . '/WikiSEO.i18n.php';
+	wfWarn(
+		'Deprecated PHP entry point used for WikiSEO extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	global $wgExtensionCredits, $wgAutoloadClasses, $wgHooks;
+
+	// Display extension properties on MediaWiki.
+	$wgExtensionCredits['parserhook'][] = array(
+		'path' => __FILE__,
+		'name' => 'WikiSEO',
+		'author' => array(
+			'Andru Vallance', '...'
 		),
-	'url' =>'https://www.mediawiki.org/wiki/Extension:WikiSEO',
-	'descriptionmsg' => 'seo-desc',
-	'license-name' => 'GPL-2.0+',
-	'version'=>'1.2.1'
-);
+		'url' => 'https://www.mediawiki.org/wiki/Extension:WikiSEO',
+		'descriptionmsg' => 'seo-desc',
+		'license-name' => 'GPL-2.0+',
+		'version' => '1.2.1'
+	);
 
-// Register extension class.
-$wgAutoloadClasses['WikiSEO'] = dirname(__FILE__) . '/WikiSEO.body.php';
+	// Register extension class.
+	$wgAutoloadClasses['WikiSEO'] = __DIR__ . '/WikiSEO.body.php';
 
-// Register extension messages and other localisation.
-$wgExtensionMessagesFiles['WikiSEOMagic'] = dirname( __FILE__ ) . '/WikiSEO.i18n.magic.php';
-$wgExtensionMessagesFiles['WikiSEO'] = dirname( __FILE__ ) . '/WikiSEO.i18n.php';
+	// Register extension messages and other localisation.
+	$wgExtensionMessagesFiles['WikiSEOMagic'] = __DIR__ . '/WikiSEO.i18n.magic.php';
+	$wgExtensionMessagesFiles['WikiSEO'] = __DIR__ . '/WikiSEO.i18n.php';
 
-//init the parser function & tag extension
-$wgHooks['ParserFirstCallInit'][] = 'WikiSEO::init';
-//check the wikitext for cached values
-$wgHooks['OutputPageBeforeHTML'][] = 'WikiSEO::loadParamsFromWikitext';
-//$wgHooks['OutputPageBeforeHTML'][] = 'WikiSEO::modifyHTML';
+	//init the parser function & tag extension
+	$wgHooks['ParserFirstCallInit'][] = 'WikiSEO::init';
+	//check the wikitext for cached values
+	$wgHooks['OutputPageBeforeHTML'][] = 'WikiSEO::loadParamsFromWikitext';
 
-//set the tags
-$wgHooks['BeforePageDisplay'][] = 'WikiSEO::modifyHTML';
+	//set the tags
+	$wgHooks['BeforePageDisplay'][] = 'WikiSEO::modifyHTML';
+}
